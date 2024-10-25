097d1969905d43feeb440be604480466ed7ce53a
eduardo <edwardhenry86.eh@gmail.com>
2024-10-25 10:52:56 -0400

    Yaap-ify

---
+++ yaap_lemonadep.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2021-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from lemonadep device
+$(call inherit-product, device/oneplus/lemonadep/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+
+# Gapps
+TARGET_BUILD_GAPPS := true
+
+PRODUCT_NAME := yaap_lemonadep
+PRODUCT_DEVICE := lemonadep
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := LE2125
+
+PRODUCT_SYSTEM_NAME := OnePlus9Pro
+PRODUCT_SYSTEM_DEVICE := OnePlus9Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="OnePlus9Pro-user 14 UKQ1.230924.001 R.1695d68-52ce-52cd release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := OnePlus/OnePlus9Pro/OnePlus9Pro:14/UKQ1.230924.001/R.1695d68-52ce-52cd:user/release-keys
+
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Stuff
+#TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+#TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 3216
+TARGET_SCREEN_WIDTH := 1440
+
