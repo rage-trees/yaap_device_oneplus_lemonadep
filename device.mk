b6c50a71c89eeed73a9856b1f6b7ee5dd735c718
eduardo <edwardhenry86.eh@gmail.com>
2024-10-25 11:00:28 -0400

    rename over to yaap

--- device.mk
+++ device.mk
@@ -56,7 +56,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-yaap
 
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
