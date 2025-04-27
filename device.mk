c3c226bb5de2e348b785917079eb120591766e17
edward <edwardhenry86.eh@gmail.com>
2025-04-27 17:38:21 -0400

    initial YAAP build

--- device.mk
+++ device.mk
@@ -29,23 +29,24 @@ TARGET_SCREEN_HEIGHT := 3216
 TARGET_SCREEN_WIDTH := 1440
 
 # Camera
-$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
+
+# Camera
+#$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
 
 # Display
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_4630946292801050754.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946292801050754.xml
-    
-# Dolby
-TARGET_USES_DOLBY := true  
 
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
     com.android.nfc_extras \
     Tag
+
+PRODUCT_PACKAGES += libpenguin
+
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml \
